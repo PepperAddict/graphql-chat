1cf5906ed24955d4d4882354e0421a8a0ecb72e4
unknown <jenearly@gmail.com>
2021-04-24 16:56:27 -0400

    fix for webpack backend being called

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const path = require("path");
 const webpack = require('./middleware/webpack')(app)
 
 
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'))
 });
--- backend/middleware/webpack.js
+++ backend/middleware/webpack.js
@@ -1,13 +1,12 @@
 module.exports = function (app) {
   const webpack = require("webpack");
-    const webpackConfig = require("../../config/webpack.config.js");
+  const webpackConfig = require("../../config/webpack.config.js");
+  const webpackCompiler = webpack(webpackConfig);
 
   //for hot reloading
   const webpackHotMiddleware = require("webpack-hot-middleware");
   app.use(webpackHotMiddleware(webpackCompiler));
 
   const webpackDevMiddleware = require("webpack-dev-middleware");
-  const webpackCompiler = webpack(webpackConfig);
   app.use(webpackDevMiddleware(webpackCompiler, webpackConfig.devServer));
-
 };
